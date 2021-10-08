2c4cf0eb3f1c4f5a36bc9e665ade7a41c38d5eb9
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-09 00:16:20 +0100

    update script sql

--- utils/sql/script_sql.sql
+++ utils/sql/script_sql.sql
@@ -5,9 +5,9 @@ default collate utf8_unicode_ci;
 create table tb_categorie(
 id int PRIMARY KEY auto_increment,
 name varchar(45) not null,
-status bool default true,
-createdat datetime default current_timestamp on update now(),
-UpdatedAt timestamp default current_timestamp on update current_timestamp
+status bool default true null,
+Created datetime default current_timestamp on update now(),
+Updated timestamp default current_timestamp on update current_timestamp
 )ENGINE = InnoDB;
 
 create table tb_product(
@@ -17,8 +17,8 @@ edition varchar(10),
 releasedata date not null,
 idcategorie int not null,
 status bool default true,
-createdat datetime default current_timestamp on update now(),
-UpdatedAt timestamp default current_timestamp on update current_timestamp,
+Created datetime default current_timestamp on update now(),
+Updated timestamp default current_timestamp on update current_timestamp,
 
 constraint `fk_produto_categoria` foreign key (idcategorie) references tb_categorie(id)
 );
@@ -31,7 +31,7 @@ birthdate varchar(15) not null,
 adress varchar(45) not null,
 idproduct int not null,
 status bool default true,
-createdat datetime default current_timestamp on update now(),
-UpdatedAt timestamp default current_timestamp on update current_timestamp,
+Created datetime default current_timestamp on update now(),
+Updated timestamp default current_timestamp on update current_timestamp,
 constraint `fk_user_produto` foreign key (idproduct) references tb_product(id)
 );
