5731436dc4a730a24ba573b41d113ca76294c7d7
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-10-09 01:45:48 +0100

    edit: add entitie category

--- entities/categorie.go
+++ entities/categorie.go
@@ -3,9 +3,20 @@ package entities
 import "time"
 
 type Categorie struct {
-	Id        string `json:"id"`
-	Name      string `json:"name"`
-	Status    string `json:"status"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Id      string    `json:"id"`
+	Name    string    `json:"name"`
+	Status  bool      `json:"status"`
+	Created time.Time `json:"created"`
+	Updated time.Time `json:"updated"`
+}
+
+func NewCategorie(name string) *Categorie {
+	categorie := &Categorie{
+		Name:    name,
+		Status:  true,
+		Created: time.Now(),
+		Updated: time.Now(),
+	}
+
+	return categorie
 }
